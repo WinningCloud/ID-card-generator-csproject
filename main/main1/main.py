135fe45b1000bd186a3b911dba98f2859e430c8c
farhan6389 <farhan84897@gmail.com>
2023-10-11 23:10:48 +0530

    font added to main1

--- main/main1/main.py
+++ main/main1/main.py
@@ -14,6 +14,9 @@ def save_to_csv(filename, data):
         writer = csv.writer(file)
         writer.writerow(data)
 
+
+
+
 # Input data and save to CSV
 while True:
     name, gender, age, dob = get_user_input()
@@ -29,10 +32,9 @@ from PIL import Image, ImageDraw, ImageFont
 import csv
 
 # Load the ID card template
-template = Image.open(r"x.png")
-draw = ImageDraw.Draw(template)
-font = ImageFont.truetype('Freedom-10eM.ttf', size=24)
-
+template = Image.open("template.png")
+# font = ImageFont.truetype('Freedom-10eMf', size=24)
+font = ImageFont.truetype(r'C:\Users\farha\Downloads\freedom-font\Freedom-10eM.ttf', 70)
 # Read data from the CSV file
 with open("data.csv", "r") as file:
     reader = csv.reader(file)
@@ -40,13 +42,17 @@ with open("data.csv", "r") as file:
     for row in reader:
         name, gender, age, dob = row
 
-        # Draw text on the template
-        draw.text((x, y), f"Name: {name}", fill="black", font=font)
-        draw.text((x, y), f"Gender: {gender}", fill="black", font=font)
-        draw.text((x, y), f"Age: {age}", fill="black", font=font)
-        draw.text((x, y), f"DOB: {dob}", fill="black", font=font)
+        # Create a new ID card based on the template
+        id_card = template.copy()
+        draw = ImageDraw.Draw(id_card)
+
+        # Draw text on the ID card
+        draw.text((278, 866), f"Name: {name}", fill="black", font=font)
+        draw.text((278, 926), f"Gender: {gender}", fill="black", font=font)
+        draw.text((278, 980), f"Age: {age}", fill="black", font=font)
+        draw.text((278, 1010), f"DOB: {dob}", fill="black", font=font)
 
         # Save the generated ID card
-        template.save(f"{name}_IDCard.png")
+        id_card.save(f"{name}_IDCard.png")
 
 print("ID cards generated successfully.")
